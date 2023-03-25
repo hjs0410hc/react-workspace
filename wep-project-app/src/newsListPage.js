ba933a8287c2e3540679972556c0bb9801707f8a
hjs5979 <hjs5979@gmail.com>
2023-03-25 16:26:22 +0900

    0325

--- wep-project-app/src/newsListPage.js
+++ wep-project-app/src/newsListPage.js
@@ -1,11 +1,30 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import axios from 'axios';
 
-export default function NewsListPage() {
-    const params = useParams();
-    console.log(params.word)
+export default function NewsListPage(props) {
+    const {state} = useLocation();
+    const param = useParams();
+    const [data, setData] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+    useEffect(()=>{
+        const getAsyncData = async ()=>{
+            setLoading(true);
+            const ad =  await axios.get("http://localhost:8080/main/test?wordid="+state.id)
+            setData(ad.data.data);
+            setLoading(false);
+        }
+        getAsyncData();
+        console.log(data);
+    },[param.word])
+
     return (
-
-        <p>{params.word}</p>
-        
+        <div>
+            {!loading && data.map((news)=>(
+                <p>{news.id} {news.category} {news.title} {news.url}</p>
+            ))}
+            <Link to="/">뒤로 가보셈</Link>
+        </div>
     );
   }
